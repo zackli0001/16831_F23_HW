59eea592e1525dacfc15a30934d6f621ebb71571
Zack Li <zackli1005@gmail.com>
2023-10-24 23:23:37 -0400

    Corrected q1 figure generation

--- hw3/average_logs.py
+++ hw3/average_logs.py
@@ -2,6 +2,7 @@ import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 import os
+import matplotlib.pyplot as plt
 
 # Base directory
 BASE_DIR = "./data"
@@ -17,7 +18,7 @@ def extract_data_from_event_file(dir_path, tag):
     _, step_nums, vals = zip(*event_acc.Scalars(tag))
     return step_nums, vals
 
-def average_data_from_dirs(dirs, tag):
+def average_and_std_data_from_dirs(dirs, tag):
     all_vals = []
     max_length = 0
     
@@ -34,7 +35,18 @@ def average_data_from_dirs(dirs, tag):
             all_vals[i].append(all_vals[i][-1])
     
     avg_vals = np.mean(all_vals, axis=0)
-    return avg_vals
+    std_vals = np.std(all_vals, axis=0)
+    return avg_vals, std_vals
+
+def plot_with_error_bars(ax, avg_data, std_data, labels, title):
+    for avg, std, label in zip(avg_data, std_data, labels):
+        x = range(len(avg))  # Ensure x matches the length of avg
+        ax.errorbar(x, avg, yerr=std, label=label)
+    ax.set_title(title)
+    ax.set_xlabel('Iterations / 1000')
+    ax.set_ylabel('Returns')
+    ax.legend()
+
 
 
 def write_to_tensorboard(avg_data, log_dir, tag):
@@ -44,9 +56,43 @@ def write_to_tensorboard(avg_data, log_dir, tag):
     writer.close()
 
 if __name__ == "__main__":
-    for tag in ["Train_AverageReturn", "Train_BestReturn"]:
-        dqn_avg = average_data_from_dirs(dqn_dirs, tag)
-        double_dqn_avg = average_data_from_dirs(double_dqn_dirs, tag)
+    # for tag in ["Train_AverageReturn", "Train_BestReturn"]:
+    #     dqn_avg = average_data_from_dirs(dqn_dirs, tag)
+    #     double_dqn_avg = average_data_from_dirs(double_dqn_dirs, tag)
         
-        write_to_tensorboard(dqn_avg, os.path.join(BASE_DIR, f"dqn_avg_{tag}"), tag)
-        write_to_tensorboard(double_dqn_avg, os.path.join(BASE_DIR, f"double_dqn_avg_{tag}"), tag)
+    #     write_to_tensorboard(dqn_avg, os.path.join(BASE_DIR, f"dqn_avg_{tag}"), tag)
+    #     write_to_tensorboard(double_dqn_avg, os.path.join(BASE_DIR, f"double_dqn_avg_{tag}"), tag)
+
+    fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+    
+    tags = ["Train_AverageReturn", "Train_BestReturn"]
+    
+    # DQN data
+    dqn_avg_data = []
+    dqn_std_data = []
+    for tag in tags:
+        avg, std = average_and_std_data_from_dirs(dqn_dirs, tag)
+        dqn_avg_data.append(avg)
+        dqn_std_data.append(std)
+    
+    # Double DQN data
+    double_dqn_avg_data = []
+    double_dqn_std_data = []
+    for tag in tags:
+        avg, std = average_and_std_data_from_dirs(double_dqn_dirs, tag)
+        double_dqn_avg_data.append(avg)
+        double_dqn_std_data.append(std)
+    
+    # Plot for DQN
+    plot_with_error_bars(axs[0], [dqn_avg_data[0], double_dqn_avg_data[0]], 
+                         [dqn_std_data[0], double_dqn_std_data[0]], 
+                         ["DQN", "Double DQN"], tags[0])
+    
+    # Plot for Double DQN
+    plot_with_error_bars(axs[1], [dqn_avg_data[1], double_dqn_avg_data[1]], 
+                         [dqn_std_data[1], double_dqn_std_data[1]], 
+                         ["DQN", "Double DQN"], tags[1])
+    
+    plt.tight_layout()
+    plt.savefig('q1_deliverable.png')
+    plt.show()
